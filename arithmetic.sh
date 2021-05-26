6f926c9551b077dfacdd284693dafaab96b0b648
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 10:28:16 +0530

    Add : Calculating 4th expression

--- arithmetic.sh
+++ arithmetic.sh
@@ -2,5 +2,11 @@
 read -p "Enter 1st input : " a
 read -p "Enter 2nd input : " b
 read -p "Enter 3rd input : " c
-y=`awk 'BEGIN{print '$c' + ('$a'/'$b')}'`
+w=`awk 'BEGIN{print '$a' + '$b' * '$c'}'`
+x=`awk 'BEGIN{print '$a' * '$b' + '$c'}'`
+y=`awk 'BEGIN{print '$c' + '$a' / '$b'}'`
+z=`awk 'BEGIN{print '$a' % '$b' + '$c'}'`
+echo " 1st Expression output is : $w"
+echo " 2nd Expression output is : $x"
 echo " 3rd Expression output is : $y"
+echo " 4th Expression output is : $z"
