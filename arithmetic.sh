82c71c63738edb75a8d34c7c46693650ab7d903b
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 11:03:26 +0530

    Add : Storing the result in a dictionary

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+declare -A dict
 read -p "Enter 1st input : " a
 read -p "Enter 2nd input : " b
 read -p "Enter 3rd input : " c
@@ -10,3 +11,13 @@ echo " 1st Expression output is : $w"
 echo " 2nd Expression output is : $x"
 echo " 3rd Expression output is : $y"
 echo " 4th Expression output is : $z"
+i=0
+dict[$i]=$w
+i=$(($i+1))
+dict[$i]=$x
+i=$(($i+1))
+dict[$i]=$y
+i=$(($i+1))
+dict[$i]=$z
+echo ${dict[@]}
+done
